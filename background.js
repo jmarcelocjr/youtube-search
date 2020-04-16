716ef7e0eb8ca874aaf79c290e97f8e54ba7326d
Marcelo Cerqueira <jmarcelo.cjr@gmail.com>
2020-04-16 12:54:41 -0300

    First commit

---
+++ background.js
@@ -0,0 +1,29 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        id: "default",
+        title: chrome.i18n.getMessage('default_menu'),
+        type: 'normal',
+        contexts: ['selection'],
+    });
+
+    chrome.contextMenus.create({
+        id: "edit",
+        title: chrome.i18n.getMessage('edit_menu'),
+        type: 'normal',
+        contexts: ['selection'],
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function(item, tab) {
+    let selectedText = item.selectionText;
+
+    if (item.menuItemId == 'edit') {
+        selectedText = prompt(chrome.i18n.getMessage('prompt_message'), selectedText);
+        if (selectedText == null) {
+            return;
+        }
+    }
+
+    let url = 'https://'+ chrome.i18n.getMessage('url') +'/search?q=' + selectedText;
+    chrome.tabs.create({url: url, index: tab.index + 1});
+});
